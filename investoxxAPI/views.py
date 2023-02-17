ac7aa2aacb07fec4391b89c5136153e6b62b22bc
Karan Kewat <84654445+karankewat02@users.noreply.github.com>
2023-02-18 01:24:38 +0530

    Optamized

--- investoxxAPI/views.py
+++ investoxxAPI/views.py
@@ -23,54 +23,64 @@ def predict_stock_performance(symbol):
     today = datetime.datetime.now().date()
     today = today - datetime.timedelta(days=3)
     two_months_ago = today - datetime.timedelta(days=60)
-    print(today,two_months_ago)
     url = f"https://api.polygon.io/v2/aggs/ticker/{symbol}/range/1/day/{two_months_ago}/{today}?adjusted=true&sort=asc&limit=1000&apiKey=_UQ5h1LONGUswPwxAjNXISHMSwRoWAtH"
     response = requests.get(url)
     data = response.json()
-    df = pd.DataFrame(data["results"])
-
-    # Calculate RSI
-    delta = df['c'].diff()
-    gain = delta.where(delta > 0, 0)
-    loss = -delta.where(delta < 0, 0)
-    avg_gain = gain.rolling(14).mean()
-    avg_loss = loss.rolling(14).mean()
-    rs = avg_gain / avg_loss
-    rs = rs.fillna(method='bfill')
-    df['RSI'] = 100 - (100 / (1 + rs))
+    if data['status'] == "ERROR":
+        return "Try again later"
     
-    # Calculate EMA
-    df['EMA'] = df['c'].ewm(span=14, adjust=False).mean()
 
-    # Use AI to make a prediction
-    X = df[['RSI', 'EMA']]
-    y = df['c'].shift(-1)
-    X = X.iloc[:-1,:]
-    y = y.iloc[:-1]
+    if data['queryCount'] != 0:
 
-    model = RandomForestRegressor(random_state=0)
-    model.fit(X, y)
-    future = model.predict(X.iloc[-1:,:])[0]
-    currentPriceURL = f"https://api.polygon.io/v2/aggs/ticker/{symbol}/prev?adjusted=true&apiKey=_UQ5h1LONGUswPwxAjNXISHMSwRoWAtH"
-    currentPriceResponse = requests.get(currentPriceURL)
-    currentPriceData = currentPriceResponse.json()
-    current = currentPriceData['results'][0]['c']
-    dfCurrent = df['c'].iloc[-1]
-    priceDiffrece = ((future -dfCurrent) / dfCurrent)*800
-    prediction = current + priceDiffrece
+        df = pd.DataFrame(data["results"])
+
+        # Calculate RSI
+        delta = df['c'].diff()
+        gain = delta.where(delta > 0, 0)
+        loss = -delta.where(delta < 0, 0)
+        avg_gain = gain.rolling(14).mean()
+        avg_loss = loss.rolling(14).mean()
+        rs = avg_gain / avg_loss
+        rs = rs.fillna(method='bfill')
+        df['RSI'] = 100 - (100 / (1 + rs))
         
+        # Calculate EMA
+        df['EMA'] = df['c'].ewm(span=14, adjust=False).mean()
 
-    response = {
-        'future': future,
-        'current': current,
-        'prediction': prediction,
-        'priceDiffrece': priceDiffrece
-    }
+        # Use AI to make a prediction
+        X = df[['RSI', 'EMA']]
+        y = df['c'].shift(-1)
+        X = X.iloc[:-1,:]
+        y = y.iloc[:-1]
 
-    print("Successfull",response)
+        model = RandomForestRegressor(random_state=0)
+        model.fit(X, y)
+        future = model.predict(X.iloc[-1:,:])[0]
 
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+        }
+        currentPriceURL = f"https://query1.finance.yahoo.com/v8/finance/chart/{symbol}?interval=1d&range=1d"
+        currentPriceResponse = requests.get(currentPriceURL, headers=headers)
+        currentPriceData = currentPriceResponse.json()
+        current = currentPriceData["chart"]["result"][0]["indicators"]["quote"][0]["close"][-1]
+        dfCurrent = df['c'].iloc[-1]
+        priceDiffrece = ((future -dfCurrent) / dfCurrent)*1600
+        prediction = current + priceDiffrece
+            
 
-    return response 
+        response = {
+            'future': future,
+            'current': current,
+            'prediction': prediction,
+            'priceDiffrece': priceDiffrece
+        }
+
+        print("Successfull",response)
+        return response 
+    
+    else:
+        return "No data found"
 
 
 
